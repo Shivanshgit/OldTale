dbc2013565065e353e1cdd2f81a3c1bbaca4eec5
Sergey Kashin <sergey.don.kashin@gmail.com>
2013-10-08 21:17:00 +0400

    Starting to work on a Prolog scene which is mean that it ia good time to create ScenesService + DialogService + IngameGUIService + CutScene logic + nice Scene itself :)

---
+++ Assets/Scripts/GUI/Menu/MainMenuGameDirector.js
@@ -0,0 +1,63 @@
+﻿#pragma strict
+/**
+ * @brief MainMenuGameDirector is used to process all MainMenu related requests.
+ */
+public var mainMenuEnabledAtStart = false;
+public var menusObject: GameObject;
+public var mainMenuOptions = new List.<String>();
+public var spaceBetweenButtons = 60;
+public var buttonsWidth = 100;
+public var buttonsHeight = 50;
+
+private var mainMenu: MainMenuComponent;
+private var gameDirector: GameDirector;
+
+function Start()
+{
+    var menuGUIService: GUIService;
+    menuGUIService = createGUIService(mainMenuEnabledAtStart);
+    mainMenu = menusObject.AddComponent(MainMenuComponent);
+    mainMenu.getMainMenu().GUIService = menuGUIService;
+
+    var mainGameObject = GameObject.FindGameObjectWithTag("GameDirector");
+    if (mainGameObject)
+    {
+        var directorComponent: GameDirectorComponent;
+        directorComponent = mainGameObject.GetComponent(GameDirectorComponent);
+        gameDirector = directorComponent.getGameDirector();
+    }
+}
+
+function createGUIService(bShown: boolean) : GUIService
+{
+    var result: GUIService;
+    result = new GUIService(bShown);
+    var newObject: GUIObject;
+
+    for(var i : int = 0; i < mainMenuOptions.Count; i++)
+    {
+        switch(i)
+        {
+            case 0: // New Game Button
+                newObject = new MenuButton(mainMenuOptions[i], Screen.width / 2, Screen.height / 2, buttonsWidth, buttonsHeight, onNewGamePressed);
+                break;
+            default: // for now it is only for Exit Button, but idealy
+                newObject = new MenuButton(mainMenuOptions[i], Screen.width / 2, Screen.height / 2 + spaceBetweenButtons, buttonsWidth, buttonsHeight, onExitButtonPressed);
+                break;
+         //TODO: fix this ^^^, it should be a general solution with a general callback. Enums as a Button's type? List of a callbacks just like for the names?
+        }
+        result.GUIOptions.Add(newObject);
+    }
+
+    return result;
+}
+
+function onExitButtonPressed()
+{
+    Application.Quit();
+}
+
+function onNewGamePressed()
+{
+    gameDirector.onStartNewGame();
+}
---
+++ Assets/Scripts/GameDirector/GameDirectorComponent.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+public var reachableLevelFromCurrentScene: List.<String>;
+
+/**
+ * @brief Main (Singleton?) that is be able to process all in-game logic, change
+ * what ever need to be changed and inform other services/directors (like MainMenuGameDirector)
+ * that current game's state is changed (for example paused/non-paused/etc.).
+ */
+public class GameDirector
+{
+    /// Constructor
+    function GameDirector()
+    {
+    }
+
+    /**
+     * @brief loads first game's scene.
+     */
+    function onStartNewGame()
+    {
+        Application.LoadLevel("Prolog");
+    }
+}
+
+private var gameDirector = new GameDirector();
+
+function getGameDirector(): GameDirector
+{
+    return gameDirector;
+}
